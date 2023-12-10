c49d183a038c366da240f04649d1f17d417af073
Alexey Antipov <antipov.alexei@gmail.com>
2023-12-10 23:50:19 +0100

    add api function

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    mode: 'directory',
+    routes: {
+      strategy: 'include',
+      include: ['/api/*'], // handled by custom function: functions/api/id].js
+    },
+  }),
   integrations: [tailwind()],
 });
---
+++ functions/api/generate.ts
@@ -0,0 +1,7 @@
+export const onRequestGet = async () => {
+  //   return new Response('Invalid token', {
+  //     status: 401,
+  //     headers: [],
+  //   });
+  return new Response('Hello world!');
+};

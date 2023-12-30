2aa34a0364b747d0bfde79258808f27a1f533cf5
Alexey Antipov <antipov.alexei@gmail.com>
2023-12-30 11:16:54 +0100

    add OG meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://masya.dev',
   output: 'server',
   adapter: cloudflare({
     runtime: {
